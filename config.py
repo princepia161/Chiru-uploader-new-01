4a8c116e9f0eb3d34b11fc64613a7a01b0c66cad
princepia161 <42060271+princepia161@users.noreply.github.com>
2026-04-19 22:14:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,17 +8,17 @@ BOT_TOKEN = os.environ.get("8564398983:AAHuuntq53gVgwIxQash63fatvHq-27BJTc",)
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-#OWNER = int(os.environ.get("OWNER", 7827463899))
+#OWNER = int(os.environ.get("OWNER", 890749443))
 
-#LOG = -1002159628443,
+#LOG = -1003646612944,
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
 #try:
-    #ADMINS=[7827463899]
-    #for x in (os.environ.get("ADMINS", "7827463899").split()):
+    #ADMINS=[890749443]
+    #for x in (os.environ.get("ADMINS", "890749443").split()):
         #ADMINS.append(int(x))
 #except ValueError:
         #raise Exception("Your Admins list does not contain valid integers.")
